d38162e19519ae87d898050825721cf4c4745328
Mahama Hamida <mahamahamida@gmail.com>
2024-12-06 18:49:27 +0000

    Add fetchCaseStudyDetails function to retrieve case study data from API

--- src/lib/api.js
+++ src/lib/api.js
@@ -12,7 +12,6 @@ const fetchProjects = async (path) => {
     const url = new URL(path, baseUrl);
     try {
       console.log(`Attempting to fetch from ${url.href}`);
-
       const res = await axios.get(url.href);
       const flattenedData = flattenAttributes(res.data);
       console.log(`Successfully fetched from ${url.href}`);
@@ -25,6 +24,21 @@ const fetchProjects = async (path) => {
   throw new Error("All base URLs failed to fetch data.");
 };
 
+const fetchCaseStudyDetails = async (id) => {
+  for (const baseUrl of baseUrls){
+    try {
+      console.log(`Attempting to fetch from ${baseUrl}`);
+      const res = await axios.get(`${baseUrl}api/case-studies/${id}`);
+      const flattenedData = flattenAttributes(res.data);
+      console.log(`Successfully fetched from ${baseUrl}`);
+
+      return flattenedData;
+    } catch (err) {
+      console.error(`Failed to fetch ${baseUrl}`, err);
+    }
+  }
+  throw new Error("All base URLs failed to fetch data.");
+};
 
 
-export { fetchProjects };
+export { fetchProjects,fetchCaseStudyDetails };
