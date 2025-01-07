908cf929cd819beccecc0e75f6bbde61cf2f498d
Mahama Hamida <mahamahamida@gmail.com>
2025-01-07 15:50:53 +0000

    Add headers configuration and enable React strict mode in Next.js config

--- next.config.mjs
+++ next.config.mjs
@@ -1,29 +1,44 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol:"https",
-          hostname:'res.cloudinary.com',
-          pathname:'**'
-        },
-        {
-          protocol:"https",
-          hostname:'media.giphy.com',
-          pathname:'**'
-        },
-        {
-          protocol:"https",
-          hostname:'portfolio-cms-jm8s.onrender.com',
-          pathname:'**'
-        },
-        {
-          protocol:"https",
-          hostname:'cms-0qf1.onrender.com',
-          pathname:'**'
-        }
-      ],
-    },
-  };
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: 'res.cloudinary.com',
+        pathname: '**'
+      },
+      {
+        protocol: "https",
+        hostname: 'media.giphy.com',
+        pathname: '**'
+      },
+      {
+        protocol: "https",
+        hostname: 'portfolio-cms-jm8s.onrender.com',
+        pathname: '**'
+      },
+      {
+        protocol: "https",
+        hostname: 'cms-0qf1.onrender.com',
+        pathname: '**'
+      }
+    ]
+  },
+  async headers() {
+    return [
+      {
+        source: '/:all*(webp|gif|jpg|jpeg|png|svg)', 
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable', 
+          },
+        ],
+      },
+    ];
+  }
+};
 
 export default nextConfig;
