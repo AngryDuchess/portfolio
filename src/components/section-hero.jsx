22e158e72fa30201860622f2de540b14df8d9361
Mahama Hamida <mahamahamida@gmail.com>
2025-01-07 15:23:17 +0000

    Integrate BrandSection component into HeroSection for enhanced brand display

--- src/components/section-hero.jsx
+++ src/components/section-hero.jsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import BrandSection from "./section-brands";
 
 export default function HeroSection() {
   return (
@@ -9,9 +10,7 @@ export default function HeroSection() {
       <div className="flex gap-4 flex-col max-w-4xl mx-auto">
         <div className="flex flex-col gap-4 text-3xl xl:text-[40px]">
           <div className="flex flex-row gap-2 xl:gap-0 xl:justify-between items-center flex-wrap xl:flex-nowrap">
-            <p className="  font-bold">
-              I Create Pretty &
-            </p>
+            <p className="  font-bold">I Create Pretty &</p>
             <span className="bg-gray-300 border  dark:bg-zinc-900 border-gray-400 dark:border-zinc-800 rounded-2xl w-16 h-16 sticker-shadow -rotate-6">
               <Image
                 src="/stickers/sticker-1.webp"
@@ -20,15 +19,11 @@ export default function HeroSection() {
                 alt=""
               />
             </span>
-            <p className="  font-bold">
-              Functional Experiences{" "}
-            </p>
+            <p className="  font-bold">Functional Experiences </p>
           </div>
           <div className="flex flex-row gap-3 items-center flex-wrap">
             <p className=" font-bold">Through</p>
-            <p
-              className=" font-bold leading-loose text-gradient bg-gradient-to-r from-[#0056b8] via-[#5fd4ff] to-[#0056b8]"
-            >
+            <p className=" font-bold leading-loose text-gradient bg-gradient-to-r from-[#0056b8] via-[#5fd4ff] to-[#0056b8]">
               Design & Code
             </p>
             <span className="bg-gray-300 border dark:bg-zinc-900 border-gray-400 dark:border-zinc-800 rounded-2xl w-16 h-16 sticker-shadow rotate-6">
@@ -41,54 +36,17 @@ export default function HeroSection() {
             </span>
           </div>
         </div>
-        <div className="flex flex-col">
-          <p className="text-portfolioTextLight dark:text-portfolioDarkTextLight text-left">
-            That&apos;s right! I don&apos;t only design..I code as well! I have
-            been doing this for 2 years and i make sure to create fresh and
-            polished solutions. I am constantly learning and immersing myself
-            into spaces that would help me grow as an individual. I have worked these
-            amazing clients:
+        <div className="flex flex-col text-portfolioTextLight dark:text-portfolioDarkTextLight text-left">
+          <p className="">
+            My expertise lies in creating polished, scalable experiences that
+            deliver real value to users and businesses alike. I&apos;ve
+            collaborated with diverse clients, mostly startups, to design and
+            build products that leave a lasting impression.
           </p>{" "}
           <br />
-          <div className="text-portfolioTextLight dark:text-portfolioDarkTextLight text-left">
-            <ul className="list-disc flex gap-4 xl:gap-2 flex-wrap">
-              <li className="flex gap-2 items-center">
-                <span className="flex items-center border px-3 border-gray-300 dark:border-zinc-800 rounded-full gap-2">
-                  <Image
-                    className="w-8 h-auto logos"
-                    src="https://res.cloudinary.com/dn5ks1ljf/image/upload/v1735865486/Portfolio%20assets/cloudport-logo_k1ht7s.png"
-                    width={2000}
-                    height={2000}
-                    alt=""
-                  />
-                  Cloudport
-                </span>
-              </li>
-              <li className="flex gap-2 items-center">
-                <span className="flex items-center border px-3 border-gray-300 dark:border-zinc-800 rounded-full gap-2">
-                  <Image
-                    className="w-8 h-auto logos"
-                    src="https://res.cloudinary.com/dn5ks1ljf/image/upload/v1735866891/Portfolio%20assets/touch_stack_technologies_logo-removebg-preview_okjzop.png"
-                    width={2000}
-                    height={2000}
-                    alt=""
-                  />
-                  Touchstack Technologies
-                </span>
-              </li>
-              <li className="flex gap-2 items-center">
-                <span className="flex items-center border px-3 border-gray-300 dark:border-zinc-800 rounded-full gap-2">
-                  <Image
-                    className="w-8 h-auto logos"
-                    src="https://res.cloudinary.com/dn5ks1ljf/image/upload/v1735865486/Portfolio%20assets/cloudport-logo_k1ht7s.png"
-                    width={2000}
-                    height={2000}
-                    alt=""
-                  />
-                  Fleetops & vantage
-                </span>
-              </li>
-            </ul>
+          <p className="mb-4">Proud to have collaborated with these brands:</p>
+          <div className="">
+            <BrandSection />
           </div>
         </div>
       </div>
