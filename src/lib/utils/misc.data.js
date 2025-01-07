b3bdf1ffde9c7984c8c8b88746f375b40c8ff6a9
Mahama Hamida <mahamahamida@gmail.com>
2025-01-07 15:23:29 +0000

    Add logosData utility for logo management and display

---
+++ src/lib/utils/misc.data.js
@@ -0,0 +1,39 @@
+export const logosData = [
+    {
+        id: 1,
+        logoUrl: "https://res.cloudinary.com/dn5ks1ljf/image/upload/v1736256349/Portfolio%20assets/cp_mjm2ws.png",
+        width: 500,
+        height: 500,
+        alt: "cloudport logo",
+        name: "Cloudport",
+        classnames: "logos"
+    },
+    {
+        id: 2,
+        logoUrl: "https://res.cloudinary.com/dn5ks1ljf/image/upload/v1736256349/Portfolio%20assets/fita_pstbnn.png",
+        width: 500,
+        height: 500,
+        alt: "greenhopper logo",
+        name: "Greenhopper",
+        classnames: "logos"
+
+    },
+    {
+        id: 3,
+        logoUrl: "https://res.cloudinary.com/dn5ks1ljf/image/upload/v1736256572/Portfolio%20assets/touchstack_rvfggi.png",
+        width: 500,
+        height: 500,
+        alt: "Touchstack Technologies logo",
+        name: "Touchstack Technologies",
+        classnames: "logos dark:bg-white rounded-full"
+    },
+    {
+        id: 4,
+        logoUrl: "https://res.cloudinary.com/dn5ks1ljf/image/upload/v1736256349/Portfolio%20assets/fleetops_nd1tw7.png",
+        width: 500,
+        height: 500,
+        alt: "Fleetops&Vantage logo",
+        name: "Fleetops&Vantage",
+        classnames: "logos"
+    }
+];
