c6e8aa278c6d2217ebebe67e5c127a6074026372
Mahama Hamida <mahamahamida@gmail.com>
2025-01-26 23:57:15 +0000

    Fetch home page data and pass it to components for dynamic rendering

--- src/app/page.js
+++ src/app/page.js
@@ -2,14 +2,25 @@ import AllProjects from "../components/section-all-projects.jsx";
 import About from "../components/section-about-me.jsx";
 import HeroSection from "../components/section-hero.jsx";
 import TechStack from "../components/section-tech-stack.jsx";
+import { getHomeData } from "@/lib/api.js";
 
-export default function Home() {
+export const revalidate = 30;
+
+export default async function Home() {
+  const pageData = await getHomeData('/api/home-page');
+
+  const designProjects = pageData?.data.allProjects.design_projects || [];
+  const developmentProjects = pageData?.data.allProjects.development_projects || [];
+
   return (
     <>
       <HeroSection />
-      <TechStack />
-      <AllProjects />
-      <About />
+      <TechStack TechStackSection={pageData?.data.techStack} />
+      <AllProjects
+        designProjects={designProjects}
+        developmentProjects={developmentProjects}
+      />
+      <About AboutSection={pageData?.data.aboutSection.aboutMe.content} />
     </>
   );
 }
