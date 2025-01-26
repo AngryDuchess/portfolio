e16e7dae2538ef634cc2f2ce0b62d76291e278c4
Mahama Hamida <mahamahamida@gmail.com>
2025-01-26 23:58:22 +0000

    Refactor TechStack component to accept props for logos and section details, improving flexibility and data handling

--- src/components/section-tech-stack.jsx
+++ src/components/section-tech-stack.jsx
@@ -1,25 +1,9 @@
-"use client";
-import { useEffect, useState } from "react";
-import { fetchProjects } from "../lib/api";
 import Image from "next/image";
 
 export const revalidate = 30;
 
-export default function TechStack() {
-  const [logos, setLogo] = useState([]);
-  useEffect(() => {
-    const getStack = async () => {
-      try{
-
-        const data = await fetchProjects("/api/tech-stack?populate=*");
-        setLogo(data.logos);
-      } catch (error) {
-        console.error("Failed to fetch tech stack")
-      }
-    };
-
-    getStack();
-  }, []);
+export default function TechStack({ TechStackSection }) {
+  const logos = TechStackSection?.stackLogos || [];
 
   return (
     <section
@@ -28,32 +12,31 @@ export default function TechStack() {
     >
       <div className="flex gap-8 flex-col max-w-4xl mx-auto justify-center items-center">
         <p className="text-3xl xl:text-[40px] font-bold text-portfolioTextDark dark:text-portfolioDarkTextDark">
-          Tech Stack
+          {TechStackSection?.sectionTitle}
         </p>
-        <p>
-          The tools and technologies i build with, streamline my workflow, and
-          bring my designs to life
-        </p>
+        <p>{TechStackSection?.sectionDescription}</p>
         <div className="flex gap-2 flex-wrap w-full items-center lg:items-start justify-center">
-          {logos && logos.length > 0 ?(
-
-            logos.map((logo) => (
-              <div
-              key={logo.id}
-              className="flex items-center rounded-3xl w-24 h-24 hover:bg-portfolioHover hover:bg-opacity-5 justify-center"
-              >
-              <Image
-                src={`${logo?.url}`}
-                className="w-16 h-16 rounded-xl"
-                alt={`${logo?.alternativeText}`}
-                width={150}
-                height={150}
-                />
-            </div>
-              ))
-            ):(
-              <p>Loading...</p>
-            )}
+        {logos && logos.length > 0 ? (
+            logos.map((logo) => {
+              const stackLogoItem = logo.stackLogo[0];
+              return (
+                <div
+                  key={stackLogoItem.id}
+                  className="flex items-center rounded-3xl w-24 h-24 hover:bg-portfolioHover hover:bg-opacity-5 justify-center"
+                >
+                  <Image
+                    src={stackLogoItem.url}
+                    className="w-16 h-16 rounded-xl"
+                    alt={stackLogoItem.alternativeText}
+                    width={150}
+                    height={150}
+                  />
+                </div>
+              );
+            })
+          ) : (
+            <p>Loading...</p>
+          )}
         </div>
       </div>
     </section>
