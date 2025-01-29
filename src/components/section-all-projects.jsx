1450428b5755eeb51c6d975b10072e218b309af1
Mahama Hamida <mahamahamida@gmail.com>
2025-01-29 22:29:16 +0000

    Update AllProjects component to improve grid layout for design projects

--- src/components/section-all-projects.jsx
+++ src/components/section-all-projects.jsx
@@ -49,7 +49,7 @@ export default function AllProjects({ designProjects, developmentProjects }) {
         </div>
         <>
           {isActive === "Design" && (
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-x-3 gap-y-8 w-full">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-3 gap-y-8 w-full">
               {designProjects.map((project, index) => (
                 <DesignCard key={project.id} project={project} index={index} />
               ))}
