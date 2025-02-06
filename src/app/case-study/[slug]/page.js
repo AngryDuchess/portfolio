c441776e75623390640a3e36d3ea27cc64f94723
Mahama Hamida <mahamahamida@gmail.com>
2025-02-06 13:00:09 +0000

    Refactor CaseStudy component to improve layout, enhance responsiveness, and update styling

--- src/app/case-study/[slug]/page.js
+++ src/app/case-study/[slug]/page.js
@@ -5,97 +5,72 @@ import { fetchCaseStudyDetails } from "@/lib/api";
 export const revalidate = 30;
 
 const CaseStudy = async ({ searchParams }) => {
-  const query = (await searchParams).id
-  const color = (await searchParams).color
-  const data = await fetchCaseStudyDetails(query || '')
-  const caseStudy = data?.data
+  const query = (await searchParams).id;
+  const color = `#${searchParams.color || ""}`;
+  const data = await fetchCaseStudyDetails(query || "");
+  const caseStudy = data?.data;
   return (
-    <>
-      {/* Thumbnail Section */}
-      <section className="text-portfolioTextDark w-screen py-16 px-4">
+    <section
+      style={{ backgroundColor: color }}
+      className="min-h-screen w-full px-4 lg:px-0 text-portfolioTextDark dark:text-portfolioDarkTextDark"
+    >
+      <div className="flex gap-8 flex-col max-w-4xl mx-auto py-8 justify-center items-start lg:items-center">
+        <h1 className="text-4xl lg:text-6xl font-medium">{caseStudy.caseStudyTitle}</h1>
+        <p>{caseStudy.caseStudyDescription}</p>
+      </div>
+      <div className="flex gap-8 flex-col md:flex-row max-w-4xl mx-auto py-8 text-sm justify-between items-start lg:items-center border-y border-white">
+        <div>
+          <p className="text-[#5FD4FF]">Duration</p>
+          <p className="">{caseStudy?.aboutProject.projectDuration}</p>
+        </div>
+        <div>
+          <p className="text-[#5FD4FF]">Platform</p>
+          <p className="">{caseStudy?.aboutProject.platform}</p>
+        </div>
+        <div>
+          <p className="text-[#5FD4FF]">Role</p>
+          <p className="">{caseStudy?.aboutProject.role}</p>
+        </div>
+        <div>
+          <p className="text-[#5FD4FF]">Client</p>
+          <p className="">{caseStudy?.aboutProject.clientName}</p>
+        </div>
+      </div>
+      <section className="py-16">
         <Image
-          className="rounded-3xl w-full max-w-3xl mx-auto"
-          src={caseStudy?.thumbnail}
+          className="rounded-3xl w-full max-w-5xl mx-auto"
+          src={caseStudy?.thumbnail.url}
           width={2000}
           height={2000}
-          alt={caseStudy?.title}
+          alt={caseStudy?.caseStudyTitle}
         />
-      </section>
-
-      {/* Project Details Section */}
-      <section className="text-portfolioTextDark dark:text-portfolioDarkTextDark w-screen px-4">
-        <div className="grid grid-cols-2 lg:grid-cols-4 gap-y-4 justify-between max-w-3xl mx-auto p-4 rounded-xl shadow-md bg-[#fafafa] dark:bg-portfolioDarkBackground border-b border-[#CCCCCC]">
-          <div className="flex flex-col items-start">
-            <p className="text-sm font-semibold text-portfolioTextLight">
-              Duration
-            </p>
-            <p className="text-sm">{caseStudy?.projectDuration}</p>
-          </div>
-          <div className="flex flex-col items-start">
-            <p className="text-sm font-semibold text-portfolioTextLight">
-              Platform
-            </p>
-            <p className="text-sm">{caseStudy?.platform}</p>
-          </div>
-          <div className="flex flex-col items-start">
-            <p className="text-sm font-semibold text-portfolioTextLight">Role</p>
-            <p className="text-sm">{caseStudy?.role}</p>
-          </div>
-          <div className="flex flex-col items-start">
-            <p className="text-sm font-semibold text-portfolioTextLight">
-              Client
-            </p>
-            <p className="text-sm">{caseStudy?.clientName}</p>
-          </div>
-        </div>
       </section>
 
       {/* Case Study Body Section */}
-      <section className="text-portfolioTextDark dark:text-portfolioDarkTextDark w-screen py-16 px-4">
-        <div className="flex gap-20 flex-col max-w-3xl mx-auto">
+      <section className="pb-16 flex flex-col items-center leading-8">
+        <div className="max-w-3xl">
+
           <BlockRendererClient content={caseStudy?.caseStudyBody} />
         </div>
-      </section>
-
-      {/* Final Mockups Section */}
-      <section className="text-portfolioTextDark dark:text-portfolioDarkTextDark text-left w-screen py-16 px-4">
-        <div className="flex gap-20 flex-col max-w-3xl mx-auto">
-          <h1 className="font-bold text-6xl">Final Mockups</h1>
-          {caseStudy?.finalMockups ? (
-            <video
-              className="w-full max-h-64"
-              controls
-              autoPlay
-              muted
-              loop
-              src={caseStudy?.finalMockups}
-            >
-              Your browser does not support this video.
-            </video>
-          ) : (
+       </section>
+       <div className="flex gap-8 flex-col max-w-4xl mx-auto pt-8 pb-20 justify-center items-start lg:items-center">
+        <h1 className="text-4xl lg:text-6xl font-medium">Final Mockups</h1>
+        <div className="flex flex-wrap gap-8 justify-center">
+          {caseStudy?.mockups?.map((mockup, index) => (
             <Image
-              src={caseStudy?.finalMockups}
+              key={index}
+              className="rounded-3xl w-full max-w-3xl mx-auto"
+              src={mockup.url}
               width={2000}
               height={2000}
-              alt="Final Mockups"
+              alt={`Mockup ${index + 1}`}
             />
-          )}
+          ))}
         </div>
-      </section>
-
-      {/* Thanks Image Section */}
-      <section className="text-portfolioTextDark dark:text-portfolioDarkTextDark w-screen py-16 px-4">
-        <Image
-          className="rounded-3xl w-full max-w-3xl mx-auto"
-          src={caseStudy?.thanksImage}
-          width={2000}
-          height={2000}
-          alt="Thanks"
-        />
-      </section>
-    </>
+        <p>Thanks for reading! 😘</p>
+      </div>
+    </section>
   );
-  // return (<div>Hello world</div>)
 };
 
 export default CaseStudy;
