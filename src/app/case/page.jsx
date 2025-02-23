1440182cddff5ff77872489fadb349353ef2292a
Mahama Hamida <mahamahamida@gmail.com>
2025-02-23 01:52:48 +0000

    Add Cases component to embed Figma presentation

---
+++ src/app/case/page.jsx
@@ -0,0 +1,13 @@
+export default function Cases() {
+  return (
+    <div className="h-screen w-screen flex items-center justify-center">
+      <iframe
+        style={{ border: "1px solid rgba(0, 0, 0, 0.1)" }}
+        width="100%"
+        height="100%"
+        src="https://embed.figma.com/deck/hp6dxBfph23ZV104ViyrHu/Dennis-Law-presentation?node-id=9-360&scaling=min-zoom&content-scaling=fixed&page-id=0%3A1&embed-host=share"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
+}
