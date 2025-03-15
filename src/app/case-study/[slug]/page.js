cdbc0e04e1f543dda81197bd53214256970dab55
Mahama Hamida <mahamahamida@gmail.com>
2025-03-15 23:45:36 +0000

    Refactor Case Study component to simplify conditional rendering and improve readability

--- src/app/case-study/[slug]/page.js
+++ src/app/case-study/[slug]/page.js
@@ -22,7 +22,7 @@ const CaseStudy = async ({ searchParams }) => {
         <ArrowLeft color="#ffffff" size={14} />
         Back to Home
       </Link>
-      {caseStudy?.caseStudyLink ? (
+      {caseStudy?.caseStudyLink && (
         <div className="h-screen w-screen flex items-center justify-center">
           <iframe
             style={{ border: "1px solid rgba(0, 0, 0, 0.1)" }}
@@ -32,68 +32,6 @@ const CaseStudy = async ({ searchParams }) => {
             allowFullScreen
           ></iframe>
         </div>
-      ) : (
-        <section
-          style={{ backgroundColor: color }}
-          className="min-h-screen w-full px-4 lg:px-0 text-portfolioDarkTextDark pt-6 lg:pt-24 relative"
-        >
-          <div className="flex gap-8 flex-col max-w-4xl mx-auto py-8 justify-center items-start lg:items-center">
-            <h1 className="text-4xl lg:text-6xl font-medium">
-              {caseStudy.caseStudyTitle}
-            </h1>
-            <p>{caseStudy.caseStudyDescription}</p>
-          </div>
-          <div className="flex gap-8 flex-col md:flex-row max-w-4xl mx-auto py-8 text-sm justify-between items-start lg:items-center border-y border-white">
-            <div>
-              <p className="text-[#5FD4FF]">Duration</p>
-              <p className="">{caseStudy?.aboutProject.projectDuration}</p>
-            </div>
-            <div>
-              <p className="text-[#5FD4FF]">Platform</p>
-              <p className="">{caseStudy?.aboutProject.platform}</p>
-            </div>
-            <div>
-              <p className="text-[#5FD4FF]">Role</p>
-              <p className="">{caseStudy?.aboutProject.role}</p>
-            </div>
-            <div>
-              <p className="text-[#5FD4FF]">Client</p>
-              <p className="">{caseStudy?.aboutProject.clientName}</p>
-            </div>
-          </div>
-          <section className="py-16">
-            <Image
-              className="rounded-3xl w-full max-w-5xl mx-auto"
-              src={caseStudy?.thumbnail.url}
-              width={2000}
-              height={2000}
-              alt={caseStudy?.caseStudyTitle}
-            />
-          </section>
-
-          {/* Case Study Body Section */}
-          <section className="pb-16 flex flex-col items-center leading-8 text-portfolioDarkTextLight">
-            <div className="max-w-3xl">
-              <BlockRendererClient content={caseStudy?.caseStudyBody} />
-            </div>
-          </section>
-          <div className="flex gap-8 flex-col max-w-4xl mx-auto pt-8 pb-28 justify-center items-start lg:items-center">
-            <h1 className="text-4xl lg:text-6xl font-medium">Final Mockups</h1>
-            <div className="flex flex-wrap gap-8 justify-center">
-              {caseStudy?.mockups?.map((mockup, index) => (
-                <Image
-                  key={index}
-                  className="rounded-3xl w-full max-w-3xl mx-auto"
-                  src={mockup.url}
-                  width={2000}
-                  height={2000}
-                  alt={`Mockup ${index + 1}`}
-                />
-              ))}
-            </div>
-            <p className="">Thanks for reading! 😘</p>
-          </div>
-        </section>
       )}
     </>
   );
